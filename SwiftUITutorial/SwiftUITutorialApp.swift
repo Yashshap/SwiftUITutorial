96ff40959c8dfbf7d537bf921080764237ac522c
yash <yash.shapane@Logituit.com>
2025-02-19 20:56:51 +0530

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Apple on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
